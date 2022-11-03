bf1d505b8a9d71f09972bb08f821f7f51b622065
Samey Sabbagh <Samey@sameysabbagh.com>
2022-11-02 20:14:55 -0400

    removed non fucnctoning JS

--- script.js
+++ script.js
@@ -65,27 +65,6 @@ function setGame() {
     }
 }
 
-var minutesLabel = document.getElementById("minutes");
-var secondsLabel = document.getElementById("seconds");
-var totalSeconds = 0;
-setInterval(setTime, 1000);
-
-function setTime() {
-  ++totalSeconds;
-  secondsLabel.innerHTML = pad(totalSeconds % 60);
-  minutesLabel.innerHTML = pad(parseInt(totalSeconds / 60));
-}
-
-function pad(val) {
-  var valString = val + "";
-  if (valString.length < 2) {
-    return "0" + valString;
-  } else {
-    return valString;
-  }
-}
-
-
 
 // seleting a number from the number row to use on the board
 function selectNumber(){
